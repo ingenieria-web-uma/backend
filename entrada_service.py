e6d33a4ff1d41b06c13f192cda606b2be193d288
scrpti <pedroscarpati@uma.es>
2024-10-29 16:51:45 +0100

    Actualizacion del backend en flask

--- entrada_service.py
+++ entrada_service.py
@@ -1,11 +1,16 @@
 from flask import Blueprint, jsonify, request
 import pymongo
+from bson import ObjectId  
 
 entrada_bp = Blueprint('entrada_bp', __name__)
+
+#Cuando entramos a /entradas accederemos aqui
 
 @entrada_bp.route("/")
 def hello_world():
     return "<a href='http://127.0.0.1:5000/entradas/g'>CLICK AQUI PARA VER TODOS LAS ENTRADAS</a>"
+
+# entradas/s?t=x&d=y con x = titulo e y = descripcion para subir entradas
 
 @entrada_bp.route("/s", methods = ['GET'])
 def upload_entry():
@@ -20,6 +25,8 @@ def upload_entry():
 
     return "<p>En esta pagina se cargaran los usuarios a la db " + t + " con descripcion " + d + " </p>"
 
+# entradas/u?t=x&d=y con x = titulo e y = descripcion para actualizar entradas
+
 @entrada_bp.route("/u", methods = ['GET'])
 def update_entry():
     t = request.args.get('t')
@@ -33,14 +40,32 @@ def update_entry():
 
     return "<p>La entrada con titulo " + t + " se ha actualizado correctamente"
 
+# entradas/g para obtener todas las entradas
+# entradas/g?t=x con x = titulo para obtener todas una entrada en especifica
+
 @entrada_bp.route("/g")
-def get_users():
-    entradas = db.entradas.find()
-    resultado = [{"id": str(entrada["_id"]), "titulo": entrada["titulo"], "descripcion": entrada["descripcion"]} for entrada in entradas]
-    return jsonify(resultado)
+def get_entry():
+    
+    t = request.args.get('t')
 
+    if t: #Si existe una entrada con titulo => t buscamos parametrizadamente, si es null devolvemos todos y si no existe pues error
+        entrada = db.entradas.find_one({"titulo": t})
 
-
+        if entrada:
+            resultado = {
+                "titulo": entrada["titulo"],
+                "descripcion": entrada["descripcion"]
+            }
+            return jsonify(resultado)
+        
+        else:
+            return "<p>Entrada no encontrada</p>"
+        
+    else: #Si t no existe pues devolvemos todos
+        entradas = db.entradas.find()
+        resultado = [{"id": str(entrada["_id"]), "titulo": entrada["titulo"], "descripcion": entrada["descripcion"]} for entrada in entradas]
+        return jsonify(resultado)
+    
 # Configuración de MongoDB
 client = pymongo.MongoClient('mongodb+srv://admin:adminadmin@cluster0.gdoyn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 db = client.laWiki

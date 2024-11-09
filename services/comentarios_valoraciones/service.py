e8f1762df3e3fd7ec5c31558ce0e48d3023b2856
migueel15 <miguelangeldorado10@gmail.com>
2024-11-09 19:38:35 +0100

    comentarios

--- services/comentarios_valoraciones/service.py
+++ services/comentarios_valoraciones/service.py
@@ -2,6 +2,7 @@ import json
 import os
 
 import pymongo
+import requests
 from bson import json_util
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
@@ -15,65 +16,70 @@ comentario_bp = Blueprint('comentario_bp', __name__)
 # Configuración de MongoDB
 client = pymongo.MongoClient(MONGO_URL)
 db = client.laWiki
-coleccion = db.entradas
 comentarios = db.comentarios
 
 # MicroServicio de COMENTARIOS
 
-# obtener todos los comentarios de un slug específico
-@comentario_bp.route("/<slug>/comments", methods = ['GET'])
-def view_comments(slug):
-    nombre = slug
-    entrada = coleccion.find_one({"slug": nombre})
-    if not entrada:
-        return jsonify({"error": "Entrada no encontrada"}), 400 #cambiar cuando sepamos el formato que vamos a usar
-    idEntrada = entrada["_id"]
-    comentarios_slug = comentarios.find({"idEntrada" : idEntrada})
-    comentarios_json = json.loads(json_util.dumps(comentarios_slug))
+# GET /comentarios
+@comentario_bp.route("/", methods = ['GET'])
+def view_comments():
+    idUsuario = request.args.get("idUsuarioRedactor")
+    idEntrada = request.args.get("idEntrada")
+    contenido = request.args.get("contenido")
+    query = {}
+
+    if idUsuario:
+        query["idUsuarioRedactor"] = {"$regex": idUsuario, "$options": "i"}
+    if idEntrada:
+        query["idEntrada"] = ObjectId(idEntrada)
+    if contenido:
+        query["contenido"] = ObjectId(contenido)
+
+    comentarios_data = comentarios.find(query)
+    comentarios_json = json.loads(json_util.dumps(comentarios_data))
     return jsonify(comentarios_json)
 
-# insertar un comentario a una entrada especifica
-@comentario_bp.route("/<slug>/comments", methods = ['POST'])
-def create_comments(slug):
-    nombre = slug
-    entrada = coleccion.find_one({"slug": nombre})
-    if not entrada:
-        return jsonify({"error": "Entrada no encontrada"}), 404
+# POST /comentarios
+@comentario_bp.route("/", methods = ['POST'])
+def create_comments():
+    datos = request.json
+    if not datos:
+        return jsonify({"error": "Datos no válidos"}), 400
+    try:
+        datos["idUsuarioRedactor"] = ObjectId(datos["idUsuarioRedactor"])
+        datos["idEntrada"] = ObjectId(datos["idEntrada"])
+    except Exception as e:
+        return jsonify({"error": f"Error al convertir los ID: {str(e) }"}), 400
     
-    idEntrada = entrada["_id"]
-    datos = request.json
+    comentarios.insert_one(datos)
+    return jsonify({"message": f"Comentario creado correctamente"})
 
-    if datos:
-        datos["idEntrada"] = idEntrada
-        comentarios.insert_one(datos)
-        return f"<p>El comentario ha sido publicado correctamente</p>"
-    else:
-        return jsonify({"error": "Datos no validos"}), 400
 
-# borrar un comentario de una entrada buscando por id del comentario.
-@comentario_bp.route("/<slug>/comments", methods = ['DELETE'])
-def delete_comments(slug):
-    datos = request.json
-    if datos:
-       result = comentarios.find_one_and_delete({"_id":ObjectId(datos["id"])})
-       if result:
-        return f"Comentario {datos["id"]} borrado con exito"
-       else:
-           return jsonify({"error": "Comentario no encontrado"}), 400
+# DELETE /comentarios
+@comentario_bp.route("/", methods = ['DELETE'])
+def delete_comments(id):
+    filtro = {"_id": ObjectId(id)}
+    comentario = comentario.find_one(filtro)
+    if comentario:
+        comentario.delete_one(filtro)
+        return jsonify({"message": f"Comentario con ID {id} eliminado correctamente"}), 200
     else:
-        return f"Comentario {datos["id"]} no encontrado. No se ha podido borrar."
+        return jsonify({"error": "Comentario no encontrado"}), 404
 
 # actualiza un comentario de una entrada
-@comentario_bp.route("/<slug>/comments", methods = ['PUT'])
-def update_comments(slug):
-    idFiltro = request.json["id"]
-    if not idFiltro:
-        return jsonify({"error", "ID de comentario no proporcionado"}), 400
+@comentario_bp.route("/", methods = ['PUT'])
+def update_comments(id):
     datos = request.json
-    del datos["id"]
-    actualizado = {"$set": datos}
-    if datos:
-        comentarios.find_one_and_update({"_id":ObjectId(idFiltro)}, actualizado)
-        return f"Comentario {idFiltro} actualizado con exito"
-    else:
-        return f"Comentario {idFiltro} no encontrado. No se ha podido actualizar"
+    if not datos:
+        return jsonify({"error": "Datos no válidos"}), 400
+    filtro = {"_id": ObjectId(id)}
+    entrada_existente = comentarios.find_one(filtro)
+    if not entrada_existente:
+        return jsonify({"error": "Entrada no encontrada"}), 404
+    
+    comentarios.update_one(filtro, {"$set": datos})
+    return jsonify({"message": f"Comentario con ID {id} actualizado correctamente"}), 200
+
+
+
+   

0249e11d1752b9a9836b3aa1e0ea46af865107d2
pablorgu <pablor623@gmail.com>
2024-11-09 15:54:08 +0100

    control de errores comentarios

--- services/comentarios_valoraciones/service.py
+++ services/comentarios_valoraciones/service.py
@@ -25,6 +25,8 @@ comentarios = db.comentarios
 def view_comments(slug):
     nombre = slug
     entrada = coleccion.find_one({"slug": nombre})
+    if not entrada:
+        return jsonify({"error": "Entrada no encontrada"}), 400 //cambiar cuando sepamos el formato que vamos a usar
     idEntrada = entrada["_id"]
     comentarios_slug = comentarios.find({"idEntrada" : idEntrada})
     comentarios_json = json.loads(json_util.dumps(comentarios_slug))
@@ -35,32 +37,43 @@ def view_comments(slug):
 def create_comments(slug):
     nombre = slug
     entrada = coleccion.find_one({"slug": nombre})
+    if not entrada:
+        return jsonify({"error": "Entrada no encontrada"}), 404
+    
     idEntrada = entrada["_id"]
-
     datos = request.json
 
     if datos:
         datos["idEntrada"] = idEntrada
         comentarios.insert_one(datos)
         return f"<p>El comentario ha sido publicado correctamente</p>"
+    else:
+        return jsonify({"error": "Datos no validos"}), 400
 
 # borrar un comentario de una entrada buscando por id del comentario.
 @comentario_bp.route("/<slug>/comments", methods = ['DELETE'])
 def delete_comments(slug):
     datos = request.json
     if datos:
-       comentarios.find_one_and_delete({"_id":ObjectId(datos["id"])})
-       return f"Comentario {datos["id"]} borrado con exito"
-    return f"Comentario {datos["id"]} no encontrado. No se ha podido borrar."
+       result = comentarios.find_one_and_delete({"_id":ObjectId(datos["id"])})
+       if result:
+        return f"Comentario {datos["id"]} borrado con exito"
+       else:
+           return jsonify({"error": "Comentario no encontrado"}), 400
+    else:
+        return f"Comentario {datos["id"]} no encontrado. No se ha podido borrar."
 
 # actualiza un comentario de una entrada
 @comentario_bp.route("/<slug>/comments", methods = ['PUT'])
 def update_comments(slug):
     idFiltro = request.json["id"]
+    if not idFiltro:
+        return jsonify({"error", "ID de comentario no proporcionado"}), 400
     datos = request.json
     del datos["id"]
     actualizado = {"$set": datos}
     if datos:
         comentarios.find_one_and_update({"_id":ObjectId(idFiltro)}, actualizado)
         return f"Comentario {idFiltro} actualizado con exito"
-    return f"Comentario {idFiltro} no encontrado. No se ha podido actualizar"
+    else:
+        return f"Comentario {idFiltro} no encontrado. No se ha podido actualizar"

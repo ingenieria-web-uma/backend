f2e56e5b4096b504ad8f493c996df0d83db91527
Pablo Robles <100775570+pablormsn@users.noreply.github.com>
2024-11-20 22:59:49 +0100

    Actualizar app.py

--- services/usuarios/app.py
+++ services/usuarios/app.py
@@ -1,5 +1,6 @@
 import os
 
+import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from .usuarios import usuarios_router
@@ -14,3 +15,10 @@ app.include_router(usuarios_router)
 @app.get("/")
 def main_route():
     return f"<a href='http://localhost:{os.getenv('SERVICE_USUARIOS_PORT')}/usuarios'>CLICK AQUI PARA IR AL APARTADO DE LOS USUARIOS</a>"
+
+# Ejecutar la aplicación
+if __name__ == "__main__":
+        puerto = os.getenv("SERVICE_USUARIOS_PORT")
+        if puerto:
+            puerto = int(puerto)
+            uvicorn.run("services.usuarios.app:app", host="0.0.0.0", port=puerto, reload=True)

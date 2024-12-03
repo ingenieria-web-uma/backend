bdb503c02badc9cecaca75eaa827c461146b00ec
scrpti <pedroscarpati@uma.es>
2024-12-03 09:36:46 +0100

    subir_archivos conectado a la gateway

--- services/archivos/archivos.py
+++ services/archivos/archivos.py
@@ -42,7 +42,7 @@ async def subir_archivo(archivo: UploadFile):
         # Insert into MongoDB
         archivos.insert_one(ArchivoNew(**archivo_res).model_dump())  # Assuming ArchivoNew is a Pydantic model
         
-        return JSONResponse(content={"mensaje": f"Archivo con nombre {nombre} subido exitosamente"}, status_code=201)
+        return JSONResponse(content={"mensaje": f"Archivo con URL {url} subido exitosamente"}, status_code=201)
     except Exception as e:
         print(f"Error uploading file: {e}")
         raise HTTPException(status_code=500, detail=f"Error al subir el archivo, {e}")
